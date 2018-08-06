716b666fa207b3212d372c8ada7698ef4955cb00
Borislav Ivanov <borogl@gmail.com>
2018-08-06 16:45:43 -0700

    Add toggle flags for lb scheme and type

--- lobo/main.py
+++ lobo/main.py
@@ -12,28 +12,36 @@ PAGE_SIZE = 100
 
 @click.command()
 @click.version_option(version=version, message=version)
+@click.option('-s', '--scheme', is_flag=True, help='Show scheme')
+@click.option('-t', '--lb-type', is_flag=True, help='Show type')
 @click.option('-p', '--profile', help='AWS profile')
 @click.option('-r', '--region', help='AWS region')
-def cli(region, profile):
+def cli(region, profile, scheme, lb_type):
     session = create_boto_session(region, profile)
     elb = create_boto_client(session, 'elb')
     elbv2 = create_boto_client(session, 'elbv2')
 
     with click_spinner.spinner():
-        elb_lbs = describe_load_balancers_elb(elb)
-        elbv2_lbs = describe_load_balancers_elbv2(elbv2)
-        lbs = elb_lbs + elbv2_lbs
-        lb_names = [x['LoadBalancerName'] for x in lbs]
-        lbs_name_max_len = len(max(lb_names, key=len))
+        lbs = describe_all_load_balancers(elb, elbv2)
+        name_pad = lb_name_max_len(lbs)
 
     for lb in sorted(lbs, key=lambda k: k['LoadBalancerName']):
-        print('{name:{name_pad}}  {scheme:15}  {type:11}'.format(
-            name=lb['LoadBalancerName'],
-            name_pad=lbs_name_max_len,
-            scheme=lb['Scheme'],
-            type=lb.get('Type', 'classic')
-        )
-        )
+        template = '{name:{name_pad}}'
+        params = {
+            'name': lb['LoadBalancerName'],
+            'name_pad': name_pad,
+            'lb': lb,
+        }
+        toggled = {'scheme': scheme, 'lb_type': lb_type}
+        template, params = show_toggled_outputs(template, params, **toggled)
+
+        print(template.format(**params))
+
+
+def describe_all_load_balancers(elb, elbv2):
+    elb_lbs = describe_load_balancers_elb(elb)
+    elbv2_lbs = describe_load_balancers_elbv2(elbv2)
+    return elb_lbs + elbv2_lbs
 
 
 def describe_load_balancers_elb(client, names=[], page_size=PAGE_SIZE):
@@ -60,6 +68,32 @@ def loop_load_balancers_pager(client, params, key):
         response = client.describe_load_balancers(**params)
         lbs += response[key]
     return lbs
+
+
+def lb_name_max_len(lbs):
+    lb_names = [x['LoadBalancerName'] for x in lbs]
+    return len(max(lb_names, key=len))
+
+
+def show_toggled_outputs(template, params, **kwargs):
+    for key, value in kwargs.items():
+        if key == 'scheme' and value:
+            template, params = toggle_scheme_output(template, params)
+        elif key == 'lb_type' and value:
+            template, params = toggle_type_output(template, params)
+    return template, params
+
+
+def toggle_scheme_output(template, params):
+    template += '  {scheme:15}'
+    params['scheme'] = params['lb']['Scheme']
+    return template, params
+
+
+def toggle_type_output(template, params):
+    template += '  {type:11}'
+    params['type'] = params['lb'].get('Type', 'classic')
+    return template, params
 
 
 def create_boto_session(region, profile):
